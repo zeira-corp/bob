d047f237d4929b2dfb987c8fb81dd4c5e6113363
ph.charriere <ph.charriere@gmail.com>
2017-01-10 16:56:00 +0100

    😜 Add some features

--- scripts/bobsbrain.js
+++ scripts/bobsbrain.js
@@ -9,7 +9,7 @@ const fetch = require('node-fetch');
 module.exports =  (robot) =>  {
 
   robot.messageRoom('general', 'Hello 🌍 I am BoB')
-
+  
   robot.hear(/bob yo/, (res) => {
     res.send(`yo ${res.message.user.name}`);
   });
@@ -27,7 +27,16 @@ module.exports =  (robot) =>  {
   });
 
   robot.hear(/coucou bob (.*)/i, (res) => {
-    res.send(`🤖 ${res.match[1]} 😜`);
+    res.send(`coucou ${res.message.user.name}`);
+    fetch(`http://bob.local:8085/lcd`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        message: `coucou ${res.message.user.name}`
+      })
+    })
   });
 
   robot.hear(/(?=.*bob)(?=.*allumer)(?=.*led)(?=.*rouge)/i, (res) => {
@@ -94,6 +103,52 @@ module.exports =  (robot) =>  {
     });
 
     res.send(`🚧  ${res.message.user.name} 🤖  éteind la led blanche`);
+  });
+
+  robot.hear(/(?=.*bob)(?=.*allumer)(?=.*tout)/i, (res) => {
+    fetch(`http://bob.local:8085/led/white/switch/on`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    fetch(`http://bob.local:8085/led/blue/switch/on`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    fetch(`http://bob.local:8085/led/red/switch/on`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    res.send(`🚧  ${res.message.user.name} 🤖  allume tout`);
+  });
+
+  robot.hear(/(?=.*bob)(?=.*éteindre)(?=.*tout)/i, (res) => {
+    fetch(`http://bob.local:8085/led/white/switch/off`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    fetch(`http://bob.local:8085/led/blue/switch/off`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    fetch(`http://bob.local:8085/led/red/switch/off`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    res.send(`🚧  ${res.message.user.name} 🤖  éteindre tout`);
   });
 
   // display the response of BoB
